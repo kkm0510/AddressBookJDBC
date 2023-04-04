2a88d89312950abcd9dadaaf6b50a54b50f1c6e0
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-04-04 08:48:42 +0530

    ADDED : code to edit contact

--- src/main/java/org/bridgelabz/service/AddressBook.java
+++ src/main/java/org/bridgelabz/service/AddressBook.java
@@ -71,12 +71,58 @@ public class AddressBook {
         System.out.println(SQLOperations.getInstance().searchByState(state));
     }
 
-    public void edit() {
+    public void edit() throws SQLException {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter first name : ");
-        String firstName=sc.next();
+        String firstName=sc.nextLine();
         System.out.println("Enter last name : ");
-        String lastName=sc.next();
+        String lastName=sc.nextLine();
+        System.out.print("What do you want to edit? \n " +
+                "(1)First name (2)Last name (3)Address (4)City (5)State (6)Pin (7)Phone number (8)Email : ");
+        int choice=sc.nextInt();
         sc.nextLine();
+        switch (choice){
+            case 1->{
+                System.out.println("Enter new firstName : ");
+                String newFirstName=sc.nextLine();
+                SQLOperations.getInstance().edit("firstName", newFirstName, firstName, lastName);
+            }
+            case 2->{
+                System.out.println("Enter new lastName : ");
+                String newLastName=sc.nextLine();
+                SQLOperations.getInstance().edit("lastName", newLastName, firstName, lastName);
+            }
+            case 3->{
+                System.out.println("Enter new address : ");
+                String address=sc.nextLine();
+                SQLOperations.getInstance().edit("address", address, firstName, lastName);
+            }
+            case 4->{
+                System.out.println("Enter new city : ");
+                String city=sc.nextLine();
+                SQLOperations.getInstance().edit("city", city, firstName, lastName);
+            }
+            case 5-> {
+                System.out.println("Enter new state : ");
+                String state=sc.nextLine();
+                SQLOperations.getInstance().edit("state", state, firstName, lastName);
+            }
+            case 6->{
+                System.out.println("Enter new pin : ");
+                String pin=sc.nextLine();
+                SQLOperations.getInstance().edit("pin", pin, firstName, lastName);
+            }
+            case 7 -> {
+                System.out.println("Enter new phone number : ");
+                String phoneNumber=sc.nextLine();
+                SQLOperations.getInstance().edit("phoneNumber", phoneNumber, firstName, lastName);
+            }
+            case 8 -> {
+                System.out.println("Enter new email : ");
+                String email=sc.nextLine();
+                SQLOperations.getInstance().edit("email", email, firstName, lastName);
+            }
+            default -> System.out.println("Wrong input!!!");
+        }
     }
 }
