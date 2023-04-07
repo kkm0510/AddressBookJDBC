aea67fd11d6043a7094e19d42bb0e8edddb8d0a8
kkm0510 <kartikeya.maan1997@yahoo.com>
2023-04-07 23:49:01 +0530

    REFACTORED : code for json

--- src/main/java/org/bridgelabz/fileinput/JSONOperations.java
+++ src/main/java/org/bridgelabz/fileinput/JSONOperations.java
@@ -8,15 +8,27 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class JSONOperations implements FileIO{
+public class JSONOperations implements FileIO {
 
     private static final String INPUT_PATH = "src/main/resources/input/ABDataIn.json";
 
     public List<Contact> getData() {
+        Map<String, List<Contact>> map = getMapData();
+        return map == null ? null : map.entrySet()
+                .stream()
+                .flatMap(entry -> entry.getValue()
+                        .stream()
+                        .peek(contact -> contact.setBookName(entry.getKey())))
+                .toList();
+    }
+
+    private Map<String, List<Contact>> getMapData() {
         try (Reader reader = Files.newBufferedReader(Paths.get(INPUT_PATH))) {
             Gson gson = new Gson();
-            return gson.fromJson(reader, new TypeToken<List<Contact>>() {
+            return gson.fromJson(reader, new TypeToken<Map<String, List<Contact>>>() {
             }.getType());
         } catch (Exception e) {
             e.printStackTrace();
